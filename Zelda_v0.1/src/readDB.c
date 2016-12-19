5dad9d8b5245e5959612c7dcafd2bfddeb471245
PKaempfer <philipp.kaempfer86@gmail.com>
2016-12-19 18:49:36 +0100

    example

--- Zelda_v0.1/src/readDB.c
+++ Zelda_v0.1/src/readDB.c
@@ -311,11 +311,6 @@ struct reads* readDB(char* outDB){
 }
 
 void freeDB(struct reads* reads){
-//	for(int i=1;i<=numreads;i++){
-//		free(reads[i].seq);
-//		if(reads[i].annotation) free(reads[i].annotation);
-//	}
-//	free(reads);
 	for(int i=0;i<=numreads;i++){
 		if(reads[i].len){
 			free(reads[i].seq);
