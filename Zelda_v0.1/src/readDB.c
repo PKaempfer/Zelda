d10bee50e8b3cc4649fe90953449cf77bf735bdb
PKaempfer <philipp.kaempfer86@gmail.com>
2017-02-21 17:07:38 +0100

    BugFix

--- Zelda_v0.1/src/readDB.c
+++ Zelda_v0.1/src/readDB.c
@@ -515,7 +515,9 @@ int readFastQ_DB(char* inFile, int readNum, int jump){
 					// Name
 					if(!first){
 						// Save read (Quality Streams?)
+						printf("Before -> readlen: %i (pos: %p)\n",strlen(read),&read);
 						comp = compressRead(read);
+						printf("After -> readlen: %i (pos: %p)\n",strlen(read),&read);
 						if(comp){
 							readsList[readTotNum].len = strlen(read);
 							readsList[readTotNum].ID = readNum;
