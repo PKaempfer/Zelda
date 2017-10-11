786d040d18ee5e86c7e138ef03b1ec8d907ea7d5
PKaempfer <philipp.kaempfer86@gmail.com>
2017-10-11 15:51:52 +0200

    test

--- Zelda_v0.1/src/DBGraph_reduced_strong.c
+++ Zelda_v0.1/src/DBGraph_reduced_strong.c
@@ -40,6 +40,7 @@ void collapseEdges_strong(int i, int dest, int up,int* nodeList, int nodenum){
 	bnode = redGraph->array[i].headread;
 	anode = redGraph->array[nodeList[j]].headread;
 
+	print("WhileloopST\n");
 	while(bnode){
 //		printf("bnode: -> %i (ID: %i) flag: %i \n",bnode->pos,bnode->read->ID,bnode->flag);
 		if(bnode->pos >= nodeList[j+1]){
@@ -98,7 +99,9 @@ void collapseEdges_strong(int i, int dest, int up,int* nodeList, int nodenum){
 			else j -= 2;
 			anode = redGraph->array[nodeList[j]].headread;
 		}
+		print("Whileloop\n");
 	}
+	print("WhileloopFin\n");
 	redGraph->array[i].headread = NULL;
 }
 
